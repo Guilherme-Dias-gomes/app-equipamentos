15dadd92506762dcfb45b6286ec44681499a6ca7
Guilherme-Dias-gomes <guilhermediasgomes2@gmail.com>
2025-08-04 17:59:47 -0300

    Intregando API para criar usuario.

--- src/app/pages/adm/dashboard/page.tsx
+++ src/app/pages/adm/dashboard/page.tsx
@@ -94,6 +94,9 @@ export default function Dashboard() {
         <Link href="/pages/solicitacao/nova" className="mb-4 inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
           Nova Solicitação
         </Link>
+        <Link href="/pages/adm/usuario/novo" className="mb-4 ml-4 inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+          Criar Usuário
+        </Link>
         <Link href="/perfil/senha" className="mb-4 ml-4 inline-block bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">
           Alterar Senha
         </Link>
